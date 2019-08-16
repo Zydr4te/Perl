240c708fbf89b08965563790fcfeb8db7be3f39f
C4RnaG3 <35183248+C4RnaG3@users.noreply.github.com>
2019-08-16 01:09:07 -0700

    Update lotl.pl
    Fixed a lot of bugs and added the database connection function

--- lotl.pl
+++ lotl.pl
@@ -31,6 +31,8 @@ main();
 
 sub main {
 
+    `clear`;
+
     print "#====================================#\n";
     print "Begining recon...\n";
     #-- Get total number of directories and files
@@ -43,15 +45,20 @@ sub main {
         print "Potentially interesting file found: $_\n";
     }
     print "#====================================#\n";
-    print "Starting in-depth filtering...\n";
+    print "Starting keyword search...\n";
     print "#====================================#\n";
-    my %int = key_find(@files);
-    while (my ($f, $p) = (each %int)) {
+    my %kint = key_find(@files);
+    while (my ($f, $p) = (each %kint)) {
+      $p =~ s/^\s+|\s+$//g; #-- Gets rid of any leading or trailing spaces
         print "$p found in $f\n";
     }
     print "#====================================#\n";
+    print "Starting database connection search...\n";
+    print "#====================================#\n";
+    db_conn(@files);
+    print "#====================================#\n";
     print "Checking for sanitization...\n";
-    my @us = haz_sanitize(@files);
+    my @us = haz_sanitize(keys %kint);
     if (scalar @us == 0) {
         print "All found files are sanitized... sadface\n";
     }
@@ -93,6 +100,7 @@ sub file_count {
 
 
     return ($dircount, $filecount, @pfiles);
+
 }
 
 
@@ -125,10 +133,10 @@ sub key_find {
 
     my %key_match;
 
-    my @keywords = qw(eval 
-    passthru 
-    system 
-    $_GET 
+    my @keywords = qw(eval
+    passthru
+    system
+    $_GET
     $_POST
     $_REQUEST);
 
@@ -139,22 +147,41 @@ sub key_find {
         open(my $fh, '<', $h);
         while (my $r = <$fh>) {
             chomp($r);
-            if (grep {$r} @keywords) {
-
+            if (grep {$r =~ /$_/} @keywords) {
+              $r = false_positive($r); #-- checks for false positives
+              if (!defined $r) {
+                next;
+              }
+              else {
                 push @interesting, $h;
-                push @key_find, $r;  
-
+                push @key_find, $r;
+              }
             }
         }
         close($fh);
         next;
     }
 
-    @interesting = do {my %seen; grep { !$seen{$_}++ } @interesting}; #-- Commenting out this line somehow lets this function read the prams array from the next function... wut
+    @interesting = do {my %seen; grep { !$seen{$_}++ } @interesting};
+
+    if (scalar @interesting == 0) {
+      print "No keywords found\n";
+    }
 
     @key_match{@interesting} = @key_find;
 
     return %key_match;
+
+}
+
+sub false_positive {
+  my $f = shift;
+
+  #-- I'll add more later
+
+  if ($f =~ /eval[uation|ate]/g){
+    return undef;
+  }
 
 }
 
@@ -177,7 +204,7 @@ sub haz_sanitize {
         chomp($s);
         open (my $f, '<', $s);
         while (my $r = <$f>) {
-            if (grep {$r} @params) {
+            if (grep {$r =~ /$_/} @params) {
                 push @sanitized, $s;
             }
             else {
@@ -189,10 +216,35 @@ sub haz_sanitize {
 
     @unsanitized = do {my %seen; grep { !$seen{$_}++ } @unsanitized};
     @sanitized = do {my %seen; grep { !$seen{$_}++ } @sanitized};
-    
+
     return @unsanitized;
 }
 
+#-- Function for finding database connection strings to determine if this is vulnerable to SQLi
+sub db_conn {
 
-#-- Function for finding database connection strings for SQLi
-sub db_con {}
+  my @dbfiles = @_;
+
+  my @constring = qw (mysql_connect mysqli_connect PDO);
+
+  my %conn_find;
+
+  my @interesting;
+
+  my @db_hit;
+
+  foreach my $d (@dbfiles) {
+    open(my $fh, '<', $d);
+    while (my $r = <$fh>) {
+        chomp($r);
+        if (grep {$r =~ /.*$_/} @constring) {
+          print "Database connection found in $d\n";
+          last;
+        } elsif (grep {$r !~ /.*$_/} @constring) {
+          next;
+        }
+      }
+      close($fh);
+    }
+    print "No database connection found\n";
+}
